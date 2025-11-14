a6aa5b655bec2953b8e51dc7d04b1b0adcaf8c7f
Gabriel Gonzalez <gabrielgonzalez@gmail.com>
2025-11-13 18:49:46 -0600

    config listo y server ya se conecta a las db

--- backend_pai2/server.js
+++ backend_pai2/server.js
@@ -1,1 +1,17 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const connectDB = require('./config/db_mongo')
+const {connectPostgres} = require('./config/db_postgres')
+
+const port = process.env.PORT || 5000 
+
+connectDB() //nos conectamos a la base de datos de mongo atlas
+connectPostgres()
+
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+app.listen(port, ()=>console.log(`Servidor Iniciado en el puerto: ${port}`))
 console.log("HOLA CRAYOLA")
