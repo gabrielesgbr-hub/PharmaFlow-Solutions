0d5f6c96d7d42c6ce4cadaa462f678bd5b7dd625
Gabriel Gonzalez <gabrielgonzalez@gmail.com>
2025-11-14 12:03:50 -0600

    listo el modelo de los repotes

--- backend_pai2/models/reportesModel.js
+++ backend_pai2/models/reportesModel.js
@@ -1,1 +1,45 @@
-//utiliza monguilloDB
+//Coleccion reportes en MO¿ongoDB
+//Las referencias a la base de datos Postgres son logicas no fisicas
+
+const mongoose = require('mongoose')
+
+const reporteSchema = mongoose.Schema({
+    titulo:{
+        type: String, 
+        required: [true, "Falta el titulo"]},
+
+    contenido:{
+        type: String, 
+        required: [true, "Falta el contenido"]},
+
+    autor:{
+        // ID del usuario REAL en PostgreSQL
+        id_usuario: { 
+            type: Number, 
+            required: [true, "Falta el id del usuario"]},
+        // Nombre del usuario 
+        nombre: { 
+            type: String, 
+            required: [true, "Falta el nombre del usuario"]}
+    },
+
+    producto: {
+        // ID del producto real en PostgreSQL
+        id_producto: { 
+            type: Number, 
+            required: [true, "Falta id del producto"]},
+        nombre: { 
+            type: String, 
+            required: [true, "Falta el nombre del producto"]}
+    },
+
+    lote: {
+        // Lote EXACTO del inventario
+        type: String,
+        required: [true, "Falta el lote"]
+    }
+},{
+    timestamps:true
+})
+
+module.exports = mongoose.model('Reporte', reporteSchema)
